d52b497486866a9315ad09cc2fa0aa57864d85b2
Mky135 <Mky.Flores@hotmail.com>
2017-04-22 12:06:09 -0500

    Adds types and xp drops to monsterStats

--- src/stats/MonsterStats.java
+++ src/stats/MonsterStats.java
@@ -2,19 +2,23 @@ package stats;
 
 public enum MonsterStats
 {
-    Troll(5, 1, 1, 1), 
-    Boss(9999, 1234, 123456, 123456);
+    Troll(5, 1, 1, 1 , "Melee" , 10),
+    Boss(9999, 1234, 123456, 123456 , "Magic" , 10);
 
     public int mHP = 0;
     public int mStrength = 0;
     public int mSpeed = 0;
     public int intelligence = 0;
+    public String typeOfDamage;
+    public int maxXpDrop = 0;
 
-    MonsterStats(int Hp, int Strength, int Speed, int intelligence)
+    MonsterStats(int Hp, int Strength, int Speed, int intelligence, String typeOfDamage, int maxXpDrop)
     {
         mHP = Hp;
         mStrength = Strength;
         mSpeed = Speed;  
         this.intelligence = intelligence;
+        this.typeOfDamage = typeOfDamage;
+        this.maxXpDrop = maxXpDrop;
     }
 }
